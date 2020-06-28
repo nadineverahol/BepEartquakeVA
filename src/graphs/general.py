d01684a8a70f21556b0c6a9fb8a93b3bdc7eff02
nadinosaurusss <nadineverahol@gmail.com>
2020-06-28 10:04:23 +0200

    Improvements general graph

--- src/graphs/general.py
+++ src/graphs/general.py
@@ -2,23 +2,28 @@ import pandas as pd
 import plotly.express as px
 
 def general_graph(data, option, min_mag, n_clusters):
+
+    lon = [item['geometry']['coordinates'][0] for item in data]
+    lat = [item['geometry']['coordinates'][1] for item in data]
+    size = [item['properties']['sig'] for item in data]
+    time = [item['properties']['time'] for item in data]
+    mag = [item['properties']['mag'] for item in data]
+    place = [item['properties']['place'] for item in data]
+    sig = [item['properties']['sig'] for item in data]
+
     if option is None:
         return {}
     if option == 'Scatter':
         
-        lon = [item['geometry']['coordinates'][0] for item in data]
-        lat = [item['geometry']['coordinates'][1] for item in data]
-        size = [item['properties']['sig'] for item in data]
-        time = [item['properties']['time'] for item in data]
 
-        fig = px.scatter_mapbox(data, lat=lat, lon=lon, color=[item['properties']['mag'] for item in data], range_color=[2.5, 10],
+        fig = px.scatter_mapbox(data, lat=lat, lon=lon, color=mag, range_color=[2.5, 10],
         size=size, hover_name=time,
         color_continuous_scale='Bluered', size_max=10, zoom=0.6, opacity=0.6)
         
         return fig
     if option == 'Densitymap':
-        fig = px.scatter_mapbox(data, lat='lat', lon='lon', color='mag', range_color=[2.5, 10],
-                                    size='sig', hover_name='place',
+        fig = px.scatter_mapbox(data, lat=lat, lon=lon, color=mag, range_color=[2.5, 10],
+                                    size=sig, hover_name=place,
                                     color_continuous_scale='bluered', size_max=10, zoom=0.6, opacity=0.6)
         return fig
     
